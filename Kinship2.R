ee46a6ca534fa23b9b8052045f2f9570a948a19c
Kalicz Péter <kaliczp@gmail.com>
2025-02-23 08:05:58 +0100

    Plot to pdf with larger fonts and remove not used args

--- Kinship2.R
+++ Kinship2.R
@@ -6,7 +6,9 @@ for(nr in 2:nrow(Kalicz)){Kalicz[nr,"Sex"] <- ifelse(any(Kalicz[,"IDparent1"] ==
 pedAll <- pedigree(id=Kalicz$ID, 
                    dadid=Kalicz$IDparent1, momid=Kalicz$IDparent2,
                    sex = Kalicz$Sex) 
-
-                sex=Kalicz$sex, famid=Kalicz$ped)
 plot(pedAll, cex = 0.7)
 plot(pedAll, cex = 0.7, id = Kalicz$Name)
+
+pdf(width = 25)
+plot(pedAll, cex = 0.8, id = Kalicz$Name)
+dev.off()
