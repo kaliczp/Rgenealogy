64ae5318a890587891c85bd21f0f7289584f0d01
Kalicz Péter <kaliczp@gmail.com>
2025-04-17 00:31:01 +0200

    Remove lines with empty ID
    Emlékek azonosító nélkül törölve

--- Kinship2.R
+++ Kinship2.R
@@ -1,6 +1,7 @@
 library(kinship2)
 
 Kalicz <- read.csv2("KaliczCS.csv")
+Kalicz <- Kalicz[Kalicz$ID != "",]
 pedAll <- pedigree(id=Kalicz$ID, 
                    dadid=Kalicz$IDparent1, momid=Kalicz$IDparent2,
                    sex = Kalicz$Sex) 
