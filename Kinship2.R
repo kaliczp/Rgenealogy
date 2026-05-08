01fb8f7c881622e2b93a4c1e6d5b30bf49e55055
Péter Kalicz <kaliczp@gmail.com>
2026-05-08 20:27:04 +0200

    Quick fix with comma separator and empty parents

--- Kinship2.R
+++ Kinship2.R
@@ -1,7 +1,12 @@
 library(kinship2)
 
-Kalicz <- read.csv2("KaliczCS.csv")
+Kalicz <- read.csv2("KaliczCS.csv", sep =",")
 Kalicz <- Kalicz[Kalicz$ID != "",]
+Kalicz$Sex <- as.numeric(Kalicz$Sex)
+Kalicz$Born <- as.Date(Kalicz$Born)
+## Both or neither
+Kalicz[which(Kalicz$IDparent1 == ""), "IDparent2"] <- ""
+Kalicz[which(Kalicz$IDparent2 == ""), "IDparent1"] <- ""
 pedAll <- pedigree(id=Kalicz$ID, 
                    dadid=Kalicz$IDparent1, momid=Kalicz$IDparent2,
                    sex = Kalicz$Sex) 
